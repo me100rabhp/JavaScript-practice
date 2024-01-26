9667e53ce62dd84d8fc44924b18371fe9c840729
me100rabhp <120414828+me100rabhp@users.noreply.github.com>
2024-01-26 19:29:04 +0000

    data types

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use stricrt"; //treat all JS code as newer version
+
+
+//alert(3+3)
+//we are using nodejs, not browser
+
+console.log(3+3);
+
+console.log("saurabh");
+
+let name = "sauabh"
+let age = 21
+let isLoggedIn = false
+
+
+console.log(typeof"saurabh");
+
+console.log(typeof age);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
