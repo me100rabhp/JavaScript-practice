1fcff194729f2dd468fa4a1d60941913d9e4ff40
me100rabhp <120414828+me100rabhp@users.noreply.github.com>
2024-01-26 19:48:57 +0000

    conversion of datatypes

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,13 +1,40 @@
 let score = "33"
 
-console.log(typeof score)
+//console.log(typeof score)
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 let isLogged = 1
 
 let booleanIsLoggedIn = Boolean(isLogged);
 
-console.log(typeof booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+
+
+
+//****************Opearation**************************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "saurabh";
+let str2 = "prakash";
+
+let str3 = str1+str2;
+
+// console.log(str3)
+
+console.log("2"+2)
+console.log(2+"2")
+console.log("2"+1+2)
+console.log(2+2+"1")
