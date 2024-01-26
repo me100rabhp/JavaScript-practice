1574db32145dd90f6565934361fe339a86cb70d9
me100rabhp <120414828+me100rabhp@users.noreply.github.com>
2024-01-26 19:28:39 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1444553
+let accountEmaiol = "me100rabh@gmail.com"
+var accountPasswors = "12345"
+accountCity = "jaipur"
+let accountState;
+//accountId = 2//not allowed
+
+accountEmaiol = "avs@gmail.com"
+accountPasswors = "123495"
+accountCity = "sasaram"
+
+console.log(accountId);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId,accountEmaiol,accountPasswors,accountCity,accountState])   
