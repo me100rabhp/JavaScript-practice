799105ee4af8430473ec4ec704dd7c643801dc47
me100rabhp <120414828+me100rabhp@users.noreply.github.com>
2024-01-27 17:37:03 +0000

    datatype-summary

---
+++ 01_basic/07_datatypes(summary).js
@@ -0,0 +1,21 @@
+ //Primitive dataTypes...
+
+ //7types:  String ,Number,Boolearn,Null,Undefined,Symbol,BigInt
+
+
+ //Reference (Non Primitive)
+
+ //Array, Objects, Functions
+
+//  const score= 100;
+//  const scorevalue = 100.3
+
+//  const isLoggedIn =false;
+//  const outsideTemp = null;
+//  let userEmail;
+
+
+ const id=Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id==anotherId)
