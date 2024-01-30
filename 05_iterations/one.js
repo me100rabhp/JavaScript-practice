9d23db31c2493ade5905d2010cc66fe09f49cf27
me100rabhp <120414828+me100rabhp@users.noreply.github.com>
2024-01-30 08:13:13 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,56 @@
+//for syntax:-
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if(element == 5){
+//         // console.log("5 is best number");
+//     }
+//     // console.log(element);
+// }
+
+
+
+for (let i = 1; i <= 10; i++) {
+    //console.log(`Outer loop value: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+       //console.log(`Inner loop value ${j} and inner loop ${i}`);
+        // console.log(`${i} * ${j} = ${i*j}`);
+    }
+    // const element = array[i]; 
+}
+
+let myArray = ["flash", "batman", "superman"];
+// console.log(myArray.length);
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    // console.log(element);
+}
+
+//break and continue
+
+// for (let index = 1; index < 20; index++) {
+//     if(index == 5){
+//         console.log(`detected 5`)
+//         break;
+//        //break loop ke bahar nikal deta hai
+//     }
+// console.log(`value of i is ${index}`); 
+
+// }
+
+
+for (let index = 1; index < 20; index++) {
+    if(index == 5){
+       console.log(`detected 5`)
+        continue;
+        //continue ek barr skip kar deta hai
+    }
+console.log(`value of i is ${index}`); 
+
+}
