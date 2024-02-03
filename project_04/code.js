31ed4d1a7bc40f7d3ac09e78190344d799bd3fbc
me100rabhp <120414828+me100rabhp@users.noreply.github.com>
2024-02-03 22:17:16 +0000

    parseInt

---
+++ project_04/code.js
@@ -0,0 +1,8 @@
+const randomNumber = parseInt(Math.random() * 100 + 1);
+
+const submit = document.querySelector('#subt')
+const userInput = document.querySelector('#guessField')
+const guessSlot = document.querySelector('#guesses')
+const remaining = document.querySelector('lastResult')
+const low = document.querySelector('lastResult')
+const remaining = document.querySelector('lastResult')
