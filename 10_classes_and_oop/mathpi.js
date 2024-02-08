0e002ca76ea3bc1b5ab37da70f3508dffb81ad65
me100rabhp <120414828+me100rabhp@users.noreply.github.com>
2024-02-08 10:42:05 +0000

    m

---
+++ 10_classes_and_oop/mathpi.js
@@ -0,0 +1,29 @@
+// const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter)
+
+
+
+// // console.log(Math.PI)
+// const p = Math.PI = 5
+// console.log(p)
+
+
+
+const c = Object.create(null)
+
+const chai = {
+    name: "ginger chai",
+    price: 250,
+    isAvailable: true
+}
+
+// console.log(Object.getOwnPropertyDescriptor(chai,"name")
+// );
+
+Object.defineProperties(chai, 'name', {
+    writable: false,
+    enumerable: false
+})
+console.log(Object.getOwnPropertyDescriptor(chai,"name")
+);
